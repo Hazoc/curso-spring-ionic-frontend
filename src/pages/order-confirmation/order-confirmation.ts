e0f8f781714a694a57f2a12f2494ea5ee208a23a
Daniel Green <hazoccc@gmail.com>
2019-12-12 21:00:55 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -53,13 +54,26 @@ export class OrderConfirmationPage {
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
-        console.log(response.headers.get('location'));
+        this.codPedido = this.extractId(response.headers.get('location'));
         this.cartService.createOrClearCart();        
       },
-      error => {});
+      error => {
+        if(error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
   }
 
   back(){
     this.navCtrl.setRoot('CartPage');
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  private extractId(location: string): string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
